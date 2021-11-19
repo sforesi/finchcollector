e1517c1a4827fc1d4fac96cf9775d8b2388162b4
sforesi <sebastiano.foresi@gmail.com>
2021-11-18 21:44:16 -0500

    add css to base.css

---
+++ main_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+   # localhost:8000/
+  path('', views.home, name="home"),
+  # localhost:8000/about/
+  path('about/', views.about, name="about"),
+  # localhost:8000/finches
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello Finch BB!</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
+# def finches_index(request):
+#   return render(request, 'finches/index.html', {'finches': finches})
+
